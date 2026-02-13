fe3e324c034aa6ebe6d96fe77b5eb224cdae206c
durand54 <durandjones@mac.com>
2026-02-12 16:14:14 -0800

    updated VehicleStateOfProduction model with new naming convention dj 02122026

--- src/Models/VehicleStartOfProduction.php
+++ src/Models/VehicleStartOfProduction.php
@@ -13,12 +13,17 @@ class VehicleStartOfProduction extends Model
     protected $fillable = [
         'id',
         'vehicle_id',
-        'StartOfProduction',
-        'startOfProductionMQ',
-        'startOfProductionY',
-        'startOfProductionNotes',
+        'start_of_production',
+        'start_of_production_mq',
+        'start_of_production_y',
+        'start_of_production_notes',
         'delete_flag',
         'time_stamp',
+    ];
+
+    protected $casts = [
+        'time_stamp' => 'date',
+        'delete_flag' => 'integer',
     ];
 
     public function vehicle() {
