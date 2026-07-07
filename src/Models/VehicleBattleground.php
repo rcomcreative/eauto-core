aad6d4e069d66e3d13d14df6ccffc3fb57aa7f30
durand54 <durandjones@mac.com>
2026-07-07 15:24:27 -0700

    changes to the vehicleBattleground model dj 07072026

--- src/Models/VehicleBattleground.php
+++ src/Models/VehicleBattleground.php
@@ -12,9 +12,27 @@ class VehicleBattleground extends Model
 
     protected $table = 'vehicle_battlegrounds';
 
-    protected  $fillable = ['id','vehicle_id','FutureProduct','XToY','PresentProduct','History',
-        'PowertrainChassis','CurrentCodeName','FutureCodeName','Change1','Change2','Chang3',
-        'AmericaRegion','AsiaRegion','EuropeRegion'];
+    protected $fillable = [
+        'vehicle_id',
+        'current_code_name',
+        'future_code_name',
+        'change_1',
+        'change_2',
+        'change_3',
+        'american_region',
+        'asia_region',
+        'europe_region',
+    ];
+
+    protected $casts = [
+        'change_1' => 'decimal:1',
+        'change_2' => 'decimal:1',
+        'change_3' => 'decimal:1',
+        'american_region' => 'boolean',
+        'asia_region' => 'boolean',
+        'europe_region' => 'boolean',
+    ];
+
     public function vehicle() {
         return $this->belongsTo(Vehicle::class);
     }
